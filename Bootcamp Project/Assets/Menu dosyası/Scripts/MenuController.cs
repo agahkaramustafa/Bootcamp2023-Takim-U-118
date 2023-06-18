269b2eaecaaa415571210182be1d09496eb9700d
Gizem Ay <giizemder08@gmail.com>
2023-06-19 00:41:02 +0300

    menu2
    menu2 2.kısmı düzenliyorum

--- Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
+++ Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.Rendering;
 
 public class MenuController : MonoBehaviour
 {
-    [Header("Level To LOad")]
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("GamePlay Settings")]
+    [SerializeField] private TMP_Text controllerSenTextValue = null;
+    [SerializeField] Slider controllerSenSlider = null;
+    [SerializeField] private int defaultSen = 4;
+    public int mainControllerSen = 4;
+
+    [Header("Toggle Settings")]
+    [SerializeField] private Toggle invertYToggle = null;
+    
+    [Header("Confirmation")]
+    [SerializeField] private GameObject comfirmationPrompt = null;
+
+    [Header("Level To Load")]
     public string _newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGameDialog = null;
@@ -32,5 +51,64 @@ public class MenuController : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        controllerSenTextValue.text = sensitivity.ToString("0");
+    }
+    public void GameApply()
+    {
+        if(invertYToggle.isOn) 
+        {
+            PlayerPrefs.SetInt("masterInvertY",1);
+            //invert Y
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0);
+            StartCoroutine (ConfirmationBox());
+        }
+    }
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.");
+            VolumeApply();  
+        }
+        if(MenuType == "Gameplay")
+        {
+            controllerSenTextValue.text=defaultSen.ToString("0");
+            controllerSenSlider.value = defaultSen;
+            mainControllerSen = defaultSen;
+            invertYToggle.isOn = false;
+            GameApply();
+
+        }
+    }
+
+
+    public IEnumerator ConfirmationBox()
+    {
+        comfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        comfirmationPrompt.SetActive(false);
     }
 }

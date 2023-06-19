98dcd72aa4eaf8c51be19e2b12c001950eba67c8
Gizem Ay <giizemder08@gmail.com>
2023-06-19 14:07:12 +0300

    baştanmenü1
    menü1

--- Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
+++ Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
@@ -3,29 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using TMPro;
-using UnityEngine.Rendering;
 
 public class MenuController : MonoBehaviour
 {
-    [Header("Volume Setting")]
-    [SerializeField] private TMP_Text volumeTextValue = null;
-    [SerializeField] private Slider volumeSlider = null;
-    [SerializeField] private float defaultVolume = 1.0f;
-
-    [Header("GamePlay Settings")]
-    [SerializeField] private TMP_Text controllerSenTextValue = null;
-    [SerializeField] Slider controllerSenSlider = null;
-    [SerializeField] private int defaultSen = 4;
-    public int mainControllerSen = 4;
-
-    [Header("Toggle Settings")]
-    [SerializeField] private Toggle invertYToggle = null;
-    
-    [Header("Confirmation")]
-    [SerializeField] private GameObject comfirmationPrompt = null;
-
-    [Header("Level To Load")]
+    [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGameDialog = null;
@@ -34,10 +15,9 @@ public class MenuController : MonoBehaviour
     {
         SceneManager.LoadScene(_newGameLevel);
     }
-
-    public void LoadGameDialogYes()
+    public void loadGameDialogYes() 
     {
-        if (PlayerPrefs.HasKey("SavedLevel1"))
+    if(PlayerPrefs.HasKey("SavedLevel"))
         {
             levelToLoad = PlayerPrefs.GetString("SavedLevel1");
             SceneManager.LoadScene(levelToLoad);
@@ -47,68 +27,9 @@ public class MenuController : MonoBehaviour
             noSavedGameDialog.SetActive(true);
         }
     }
-
     public void ExitButton()
     {
         Application.Quit();
     }
 
-    public void SetVolume(float volume)
-    {
-        AudioListener.volume = volume;
-        volumeTextValue.text = volume.ToString("0.0");
-    }
-
-    public void VolumeApply()
-    {
-        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-        StartCoroutine(ConfirmationBox());
-    }
-
-    public void SetControllerSen(float sensitivity)
-    {
-        mainControllerSen = Mathf.RoundToInt(sensitivity);
-        controllerSenTextValue.text = sensitivity.ToString("0");
-    }
-    public void GameApply()
-    {
-        if(invertYToggle.isOn) 
-        {
-            PlayerPrefs.SetInt("masterInvertY",1);
-            //invert Y
-
-        }
-        else
-        {
-            PlayerPrefs.SetInt("masterInvertY", 0);
-            StartCoroutine (ConfirmationBox());
-        }
-    }
-    public void ResetButton(string MenuType)
-    {
-        if (MenuType == "Audio")
-        {
-            AudioListener.volume = defaultVolume;
-            volumeSlider.value = defaultVolume;
-            volumeTextValue.text = defaultVolume.ToString("0.");
-            VolumeApply();  
-        }
-        if(MenuType == "Gameplay")
-        {
-            controllerSenTextValue.text=defaultSen.ToString("0");
-            controllerSenSlider.value = defaultSen;
-            mainControllerSen = defaultSen;
-            invertYToggle.isOn = false;
-            GameApply();
-
-        }
-    }
-
-
-    public IEnumerator ConfirmationBox()
-    {
-        comfirmationPrompt.SetActive(true);
-        yield return new WaitForSeconds(2);
-        comfirmationPrompt.SetActive(false);
-    }
 }

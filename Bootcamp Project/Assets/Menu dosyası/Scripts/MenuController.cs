b22b520cfa8676211aa0a5cb1dce3f4a43775752
Gizem Ay <giizemder08@gmail.com>
2023-06-22 02:23:04 +0300

    menu3
    menu3

--- Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
+++ Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
+
 public class MenuController : MonoBehaviour
 {
     [Header("Volume Setting")]
@@ -18,17 +19,64 @@ public class MenuController : MonoBehaviour
     [SerializeField] private int defaultSen = 4;
     public int mainControllerSen = 4;
 
+    
     [Header("Toggle Settings")]
     [SerializeField] private Toggle invertYToggle = null;
 
+    [Header("Graphics Settings")]
+    [SerializeField] private Slider brightnessSlider = null;
+    [SerializeField] private TMP_Text brightnessTextValue = null;
+    [SerializeField] private float defualtBrightness = 1;
+
+    [Space(10)]
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullScreenToggle;
+
+
+    private int _qualityLevel1;
+    private bool _isFullScreen;
+    private float _brightnessLevel;
 
     [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
-
+    
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGameDialog = null;
+
+    [Header("Resolution Dropdowns")]
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    public void Start()
+    {
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for(int i=0; i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width== Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+      
+    }
+
+    public void SetResolutiion(int resolutiionIndex) 
+    {
+    Resolution resolution = resolutions[resolutiionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public void NewGameDialogYes()
     {
@@ -79,11 +127,60 @@ public class MenuController : MonoBehaviour
         {
             PlayerPrefs.SetInt("masterInvertY", 0);
         }
+
         PlayerPrefs.SetFloat("masterSen", mainControllerSen);
         StartCoroutine (ConfirmationBox());
     }
+
+    public void SetBrightness(float brightness)
+    {
+        _brightnessLevel = brightness;
+        brightnessTextValue.text = brightness.ToString("0.0");
+    }
+
+    public  void SetFullScreen(bool isFullscreen)
+    {
+        _isFullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel1 = qualityIndex;
+    }
+
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
+
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel1);
+        QualitySettings.SetQualityLevel(_qualityLevel1);
+
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullScreen ? 1 : 0));
+        Screen.fullScreen = _isFullScreen;
+
+        StartCoroutine (ConfirmationBox());
+    }
+
+    
+
     public void ResetButoon(string  MenuType)
     {
+        if(MenuType == "Graphics")
+        {
+            brightnessSlider.value = defualtBrightness;
+            brightnessTextValue.text = defualtBrightness.ToString("0.0");
+
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;
+            GraphicsApply();
+        }
         if (MenuType == "Audio")
         {
             AudioListener.volume = defualtVolume;

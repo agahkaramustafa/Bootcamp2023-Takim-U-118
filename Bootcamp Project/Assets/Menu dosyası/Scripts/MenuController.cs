a996658104db55e50fa0ae727d7fc3aef560e89d
Gizem Ay <giizemder08@gmail.com>
2023-06-21 19:56:15 +0300

    menü3
    menüyapılış

--- Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
+++ Bootcamp Project/Assets/Menu dosyası/Scripts/MenuController.cs
@@ -10,7 +10,19 @@ public class MenuController : MonoBehaviour
     [Header("Volume Setting")]
     [SerializeField] private TMP_Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defualtVolume = 1.0f;
 
+    [Header("Gameplay Settings")]
+    [SerializeField] private  TMP_Text controllerSenTextValue =null;
+    [SerializeField] private Slider controllerSenSlider = null;
+    [SerializeField] private int defaultSen = 4;
+    public int mainControllerSen = 4;
+
+    [Header("Toggle Settings")]
+    [SerializeField] private Toggle invertYToggle = null;
+
+
+    [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
 
     [Header("Levels To Load")]
@@ -51,6 +63,42 @@ public class MenuController : MonoBehaviour
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
         StartCoroutine(ConfirmationBox());
+    }
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        controllerSenTextValue.text = sensitivity.ToString("0");
+    }
+    public void GameApply()
+    {
+        if (invertYToggle.isOn) 
+        {
+            PlayerPrefs.SetInt("masterInvertY", 1);
+        }
+        else 
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0);
+        }
+        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+        StartCoroutine (ConfirmationBox());
+    }
+    public void ResetButoon(string  MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defualtVolume;
+            volumeSlider.value = defualtVolume;
+            volumeTextValue.text = defualtVolume.ToString("0.0");
+            VolumeApply();
+        }
+        if(MenuType == "Gameplay")
+        {
+            controllerSenTextValue.text = defaultSen.ToString("0");
+            controllerSenSlider.value = defaultSen;
+            mainControllerSen = defaultSen;
+            invertYToggle.isOn = false;
+            GameApply();
+        }
     }
 
     public IEnumerator ConfirmationBox()

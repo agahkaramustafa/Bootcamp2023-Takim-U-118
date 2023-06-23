d53f9e98d4fe1151347385574d4d7661c7dc8a2a
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2023-06-23 11:05:26 +0300

    Squashed commit of the following:
    commit 43d1ebfd253df7afade90eede60bf9ee0ed5198b
    Author: Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
    Date:   Fri Jun 23 11:00:37 2023 +0300
    
        Debugging
    
    commit 51136279af9b0a306687275cbc9ac0cdafd08cc1
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Fri Jun 23 00:28:46 2023 +0300
    
        duzenlemısmenu
    
        duzenlemısmenu
    
    commit df7c6ad2d177c36abaec018f8f10d5d20602d066
    Merge: a8f610f 1df64c3
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Thu Jun 22 22:51:05 2023 +0300
    
        Merge branch 'main' of https://github.com/aygizemay/Bootcamp2023-Takim-U-118
    
    commit a8f610fa9eee11801c5b0186415ccc1d4ff8761f
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Thu Jun 22 22:47:29 2023 +0300
    
        menu
    
        menu
    
    commit 1df64c39bba9556739a49e67c3456af5b5b9814e
    Merge: bbe9d16 0d9f0bf
    Author: Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
    Date:   Thu Jun 22 21:46:51 2023 +0300
    
        Merge branch 'main' into main
    
    commit bbe9d164abe79f42fb7e071a2b880977d5d6bc3c
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Thu Jun 22 17:45:55 2023 +0300
    
        menu
    
        son hali-kodlar
    
    commit 3a70331610c826a1321c692f76b40acc4d5b09ee
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Thu Jun 22 17:20:44 2023 +0300
    
        menuson
    
        menuson
    
    commit da67d680174e4f927f6c57f08aa90fc730bc31ef
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Thu Jun 22 14:15:42 2023 +0300
    
        menu
    
        menu
    
    commit 03169765c3656d7ef45229b9c24f77d84781f85e
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Thu Jun 22 12:43:15 2023 +0300
    
        menu
    
        menuson
    
    commit b22b520cfa8676211aa0a5cb1dce3f4a43775752
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Thu Jun 22 02:23:04 2023 +0300
    
        menu3
    
        menu3
    
    commit a996658104db55e50fa0ae727d7fc3aef560e89d
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Wed Jun 21 19:56:15 2023 +0300
    
        menü3
    
        menüyapılış
    
    commit 3d74940b8d0fe786009a5889596d699921dfd31a
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Mon Jun 19 21:38:30 2023 +0300
    
        menu2.1
    
        menu2.1
    
    commit bc5117b411f052efed5b7258f5f66668a0e84ff8
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Mon Jun 19 14:23:04 2023 +0300
    
        menu2
    
        menu
    
    commit 98dcd72aa4eaf8c51be19e2b12c001950eba67c8
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Mon Jun 19 14:07:12 2023 +0300
    
        baştanmenü1
    
        menü1
    
    commit 269b2eaecaaa415571210182be1d09496eb9700d
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Mon Jun 19 00:41:02 2023 +0300
    
        menu2
    
        menu2 2.kısmı düzenliyorum
    
    commit 07913e25f6a5ac93469ba230a14104e500e6a286
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Sun Jun 18 19:30:42 2023 +0300
    
        menu
    
        menuyapımı
    
    commit 8af5c42115485e6ba479a456448ddd678e741099
    Author: Gizem Ay <giizemder08@gmail.com>
    Date:   Fri Jun 16 11:02:20 2023 +0300
    
        deneme

--- Bootcamp Project/Assets/MenuFolder/Scripts/LoadPrefs.cs
+++ Bootcamp Project/Assets/MenuFolder/Scripts/LoadPrefs.cs
@@ -11,25 +11,15 @@ public class LoadPrefs : MonoBehaviour
     [SerializeField] private Slider volumeSlider;
 
     [Header("Graphics Settings")]
-    [SerializeField] private Slider brightnessSlider;
-    [SerializeField] private TMP_Text brightnessTextValue;
     [SerializeField] private TMP_Dropdown qualityDropdown;
     [SerializeField] private Toggle fullscreenToggle;
-
-    [Header("Gameplay Settings")]
-    [SerializeField] private TMP_Text controllerSenTextValue;
-    [SerializeField] private Slider controllerSenSlider;
-    private int mainControllerSen;
-
-    [Header("Toggle Settings")]
-    [SerializeField] private Toggle invertYToggle;
 
     private void Start()
     {
         LoadPlayerPrefs();
     }
 
-    private void LoadPlayerPrefs()
+   private void LoadPlayerPrefs()
     {
         if (PlayerPrefs.HasKey("masterVolume"))
         {
@@ -51,26 +41,6 @@ public class LoadPrefs : MonoBehaviour
             int localFullscreen = PlayerPrefs.GetInt("masterFullscreen");
             fullscreenToggle.isOn = localFullscreen == 1;
             Screen.fullScreen = fullscreenToggle.isOn;
-        }
-
-        if (PlayerPrefs.HasKey("masterBrightness"))
-        {
-            float localBrightness = PlayerPrefs.GetFloat("masterBrightness");
-            brightnessTextValue.text = localBrightness.ToString("0.0");
-            brightnessSlider.value = localBrightness;
-        }
-
-        if (PlayerPrefs.HasKey("masterSen"))
-        {
-            float localSensitivity = PlayerPrefs.GetFloat("masterSen");
-            controllerSenTextValue.text = localSensitivity.ToString("0");
-            controllerSenSlider.value = localSensitivity;
-            mainControllerSen = Mathf.RoundToInt(localSensitivity);
-        }
-
-        if (PlayerPrefs.HasKey("masterInvertY"))
-        {
-            invertYToggle.isOn = PlayerPrefs.GetInt("masterInvertY") == 1;
         }
     }
 }

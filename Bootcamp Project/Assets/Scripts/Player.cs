d1a3d06ae58c2bda35cd90e29adb958ab093f169
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2023-07-08 20:13:25 +0300

    Sword animations

--- Bootcamp Project/Assets/Scripts/Player.cs
+++ Bootcamp Project/Assets/Scripts/Player.cs
@@ -10,15 +10,53 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject horusSwordPrefab;
     [SerializeField] DissolveChilds dissolveChilds;
 
+    private float nextFireTime = 0f;
+    public static int noOfClicks = 0;
+    float lastClickedTime = 0;
+    float maxComboDelay = 1f;
+
+
     private WaitForSeconds powerUpWaitTime = new WaitForSeconds(5);
+    private Animator anim;
 
     private bool hasPowerUp;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     private void Update()
     {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        {
+            anim.SetBool("Hit1", false);
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
+        {
+            anim.SetBool("Hit2", false);
+            noOfClicks = 0;
+        }
+
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+        if (Time.time > nextFireTime)
+        {
+            if (Input.GetMouseButtonDown(0) && hasPowerUp)
+            {
+                OnClick();
+            }
+        }
+
         if(Input.GetKeyDown(KeyCode.Tab))
         {
             PowerUpProcess();
@@ -42,5 +80,24 @@ public class Player : MonoBehaviour
         powerUpParticleSystem.Stop();
         horusSwordPrefab.SetActive(!hasPowerUp);
         hasPowerUp = false;
+    }
+
+    private void OnClick()
+    {
+        lastClickedTime = Time.time;
+        noOfClicks++;
+
+        if (noOfClicks == 1)
+        {
+            anim.SetBool("Hit1", true);
+        }
+        noOfClicks = Mathf.Clamp(noOfClicks, 0, 2);
+
+        if (noOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        {
+            anim.SetBool("Hit1", false);
+            anim.SetBool("Hit2", true);
+        }
+
     }
 }

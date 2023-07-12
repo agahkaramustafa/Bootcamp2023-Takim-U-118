4f8959a8781e264d0bc367c1d9ec656417c8b25a
Gizem Ay <giizemder08@gmail.com>
2023-07-12 13:41:04 +0300

    level2tasarım
    level2tasarım

---
+++ Bootcamp Project/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DissolveExample;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] ParticleSystem powerUpParticleSystem;
+    [SerializeField] GameObject horusSwordPrefab;
+    [SerializeField] DissolveChilds dissolveChilds;
+
+    private WaitForSeconds powerUpWaitTime = new WaitForSeconds(5);
+
+    public bool HasPowerUp { get { return hasPowerUp; } }
+    private bool hasPowerUp;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            PowerUpProcess();
+        }
+    }
+
+    private void PowerUpProcess()
+    {
+        StartCoroutine(nameof(PowerUpRoutine));
+    }
+
+    IEnumerator PowerUpRoutine()
+    {
+        hasPowerUp = true;
+        powerUpParticleSystem.Play();
+        horusSwordPrefab.SetActive(hasPowerUp);
+        dissolveChilds.Respawn();
+        yield return powerUpWaitTime;
+        dissolveChilds.Dissolve();
+        yield return new WaitForSeconds(2f);
+        powerUpParticleSystem.Stop();
+        horusSwordPrefab.SetActive(!hasPowerUp);
+        hasPowerUp = false;
+    }
+}
